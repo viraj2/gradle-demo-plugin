e8a26671912cf65d927120e36726fafd495d2ffe
viraj2 <virajchhaiya1@gmail.com>
2018-10-23 18:01:45 +0530

    Initial Commit

--- src/main/java/org/example/greeting/GreetingPlugin.java
+++ src/main/java/org/example/greeting/GreetingPlugin.java
@@ -5,7 +5,7 @@ import org.gradle.api.Project;
 
 public class GreetingPlugin implements Plugin<Project> {
     public void apply(Project project) {
-        project.getTasks().create("hello", Greeting.class, (task) -> { // <1>
+        project.getTasks().create("helloworld", Greeting.class, (task) -> { // <1>
             task.setMessage("Hello");
             task.setRecipient("World");                                // <2>
         });
